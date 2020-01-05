fde7ebda7593c51c14f728dd3342884059a67f07
Tibo Clausen <tibo.clausen@gmail.com>
2020-01-05 18:54:52 +0100

    Improve heatmap script

--- heatmap_generator.py
+++ heatmap_generator.py
@@ -3,6 +3,13 @@ from PIL import Image, ImageDraw, ImageFont
 import json
 import click
 
+keyboard = [
+  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2],
+  [1.5, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, [1.5, 1.25]],
+  [1.75, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+  [1.25, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2.75],
+  [1.25, 1.25, 1.25, 6.25, 1.25, 1.25, 1.25, 1.25]
+]
 
 @click.command()
 @click.argument('layout_file', type=click.File('r'))
@@ -11,9 +18,9 @@ import click
 def cli(layout_file, frequency_file, output):
     # Parse json files
     layout_data = json.load(layout_file)
-    freq_data = json.load(frequency_file) if frequency_file else None
+    freq_data = json.load(frequency_file)['single'] if frequency_file else None
     # Generate layout
-    layout = generate_layout(layout_data, freq_data['single'])
+    layout = generate_layout(layout_data, freq_data)
 #   layout_data = process_layout(layout_data, freq_data)
     # Save image
     img = render_layout(layout)
@@ -22,44 +29,36 @@ def cli(layout_file, frequency_file, output):
 
 def generate_row(keys, freq_data):
     glyphs = []
-    sizes = []
     freq = []
 
-    next_size = None
-
     for key in keys:
-        # If it's a special "key" handle it accordingly
-        if isinstance(key, dict):
-            if 'w' in key:
-                next_size = key['w']
-        else:
-            # Get the glyph
-            glyphs.append(key)
-            # Get the size and reset it
-            size = next_size or 1
-            next_size = None
-            sizes.append(size)
-            # Get the frequency
-            freq.append(freq_data.get(key.lower(), 0))
+        # Get the glyph
+        glyphs.append(key)
 
-    return glyphs, sizes, freq
+        # Get the frequency
+        if freq_data:
+            if isinstance(key, list):
+                freq.append(freq_data.get(key[0].lower(), 0))
+            else:
+                freq.append(freq_data.get(key.lower(), 0))
+        else:
+            freq.append(0)
+
+    return glyphs, freq
 
 
 def generate_layout(layout_data, freq_data):
     glyphs = []
-    sizes = []
     freq = []
 
     # Go through every key in the layout
     for row in layout_data:
-        glyph_row, sizes_row, freq_row = generate_row(row, freq_data)
+        glyph_row, freq_row = generate_row(row, freq_data)
         glyphs.append(glyph_row)
-        sizes.append(sizes_row)
         freq.append(freq_row)
 
     return {
         'glyphs': glyphs,
-        'sizes': sizes,
         'freq': freq,
         }
 
@@ -67,18 +66,19 @@ def generate_layout(layout_data, freq_data):
 def render_layout(layout):
     # Create the image
     # TODO: The size should not be static
-    img = Image.new('RGB', (2000, 720))
+    img = Image.new('RGB', (1500, 500), 'white')
     draw = ImageDraw.Draw(img)
 
     # Find highest frquency
     max_freq = max(map(max, layout['freq']))
 
-    zipped = list(zip(layout['glyphs'], layout['sizes'], layout['freq']))
-    print(zipped[2])
+    zipped = list(zip(layout['glyphs'], keyboard, layout['freq']))
 
     # Estimate the size of a key
-    # TODO: This need to be improved
     unit = img.height / len(zipped)
+
+    # Set the padding
+    pad = 10
 
     # Prepare font
     font = ImageFont.truetype('arial.ttf', round(unit/3))
@@ -91,19 +91,74 @@ def render_layout(layout):
         keys = zip(row_keys[0], row_keys[1], row_keys[2])
         # Iterate over every key
         for key_nr, key in enumerate(keys):
+            # Calculate color
+            if key[2] == 0:
+                color = 'white'
+            else:
+                color = get_color(key[2], max_freq)
             # Calculate the effective size
-            size = unit * key[1]
-            # Calculate the rectangle
-            topleft = (offset, row_nr * unit)
-            botright = (offset + size, row_nr * unit + unit)
+            if isinstance(key[1], list):
+                size = unit * key[1][0]
+                # Calculate the rectangle
+                topleft = (offset, row_nr * unit)
+                botright = (
+                    offset + unit * max(key[1]),
+                    row_nr * unit + unit * 2
+                )
+                # Draw the key
+                points = [
+                    topleft,
+                    (topleft[0] + unit * key[1][0], topleft[1]),
+                    botright,
+                    (botright[0] - key[1][1] * unit, botright[1]),
+                    (botright[0] - key[1][1] * unit, botright[1] - unit),
+                    (topleft[0], topleft[1] + unit)
+                ]
+                draw.polygon(points, color, 'gray')
+            else:
+                size = unit * key[1]
+                # Calculate the rectangle
+                topleft = (offset, row_nr * unit)
+                botright = (offset + size, row_nr * unit + unit)
+                # Draw the key
+                draw.rectangle([topleft, botright], color, 'gray')
             # Update the offset
             offset += size
-            # Calculate color
-            alpha = round(key[2] / max_freq * 255)
-            # Draw the key
-            draw.rectangle([topleft, botright], (round(255-alpha), 255, round(255-alpha)), 'gray')
-            draw.text(topleft, key[0], 'black', font=font)
+            # Draw the glyph
+            topleft_pad = (topleft[0] + pad, topleft[1] + pad)
+            if isinstance(key[0], list):
+                # chars = [char.title() for char in key[0]]
+                chars = key[0]
+                layers = len(chars)
+                font_height = font.getsize(chars[0])[1]
+                botleft_pad = (
+                    topleft[0] + pad,
+                    topleft[1] + unit - font_height - pad
+                )
+                draw.text(botleft_pad, chars[0], 'black', font=font)
+                if layers >= 2:
+                    draw.text(topleft_pad, chars[1], 'black', font=font)
+                if layers == 3:
+                    font_size = font.getsize(chars[2])
+                    botright_pad = (
+                        botright[0] - font_size[0] - pad,
+                        botright[1] - font_size[1] - pad
+                    )
+                    draw.text(botright_pad, chars[2], 'black', font=font)
+            else:
+                # draw.text(topleft_pad, key[0].title(), 'black', font=font)
+                draw.text(topleft_pad, key[0], 'black', font=font)
+    draw.rectangle([(0, 0), (img.width - 1, img.height - 1)], None, 'grey')
+
     return img
+
+
+def get_color(val, max_val):
+    value = val / max_val
+    hue = (1 - value) * 220
+    sat = 100
+    lig = 50
+    return f'hsl({hue}, {sat}%, {lig}%)'
 
 
 if __name__ == "__main__":
